09bcc77f0b867a123a00f1573194ea82b71d046d
ZeroHzzzz <3190381602@qq.com>
2024-09-24 02:24:36 +0800

    Updata filter

--- scripts/custom_filter.js
+++ scripts/custom_filter.js
@@ -1,5 +1,5 @@
 hexo.extend.filter.register("before_post_render", function (data) {
     // 替换只有单个 # 后跟非空白字符的情况，而不影响 ## 或更多 #
-    data.content = data.content.replace(/(^|\s)#([^\s])/g, "$1\\#$2");
+    data.content = data.content.replace(/(^|\s)#([^\s#])/g, "$1\\#$2");
     return data;
 });
