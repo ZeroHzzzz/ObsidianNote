5798f4c80bb2c6ac6565cc39d2e78e23309f1f22
ZeroHzzzz <3190381602@qq.com>
2024-09-24 02:16:21 +0800

    Updata filter

--- scripts/custom_filter.js
+++ scripts/custom_filter.js
@@ -1,5 +1,5 @@
 hexo.extend.filter.register("before_post_render", function (data) {
-    // 替换所有 "#<text>" 为 "\#<text>" 以避免被解析为标题
+    // 替换所有 "#<text>" 为 "\#<text>"
     data.content = data.content.replace(/#([^\s])/g, "\\#$1");
     return data;
 });
